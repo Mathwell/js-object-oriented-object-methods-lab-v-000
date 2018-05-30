2216b4494d54b54f84d310559dee8d08eea94147
Olena Ageyeva <eageeva@hotmail.com>
2018-05-30 12:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function boardMember(name, homeState, training){
+before all function boardMember(name, homeState, training){
   this.name=name;
   this.homeState=homeState;
   this.training=training;
@@ -7,5 +7,5 @@ function boardMember(name, homeState, training){
   this.doCharity=function(){return "I like to help people."}
   this.releasePressStatement=function(){return "You will see great things from Scruber."}
   this.sayHi=function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`}
-  
+
 }
