d43b1b26e8f97442af4b193061a55bd3b6663c2e
Olena Ageyeva <eageeva@hotmail.com>
2018-05-30 12:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function boardMember(name, homeState, training){
   this.name=name;
   this.homeState=homeState;
   this.training=training;
-  this.veto=function(){
-    return "No, I must disagree"
-  }
+  this.veto=function(){return "No, I must disagree"}
+  this.approve=function(){return "You can do that!"}
+  this.doCharity=function(){return "I like to help people."}
+  this.releasePressStatement=function(){return "You will see great things from Scruber."}
+  this.sayHi=function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`}
+  
 }
